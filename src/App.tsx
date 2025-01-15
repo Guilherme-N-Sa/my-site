f34c75208779295e118e4a11ed041c08730671ae
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2025-01-15 12:41:35 -0300

    Integrate styled-components for theming and global styles in App component; remove GlobalStyles from index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import Hero from './components/Hero/Hero';
-import DetailedInfo from './components/DetailedInfo/DetailedInfo';
-import SocialBar from './components/SocialBar/SocialBar';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import Layout from "./components/Layout/Layout";
+import Hero from "./components/Hero/Hero";
+import DetailedInfo from "./components/DetailedInfo/DetailedInfo";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Hero
-        name="Guilherme Niclewicz"
-        title="Senior FullStack Developer"
-        description="I build FullStack web applications, specializing in backend development and cloud computing. Graduated in Engineering with a Master's degree in Computational Mathematics."
-      />
-      <SocialBar />
-      <DetailedInfo />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Layout>
+        <Hero
+          name="Guilherme Niclewicz"
+          title="Senior FullStack Developer"
+          description="I build FullStack web applications, specializing in backend development and cloud computing. Graduated in Engineering with a Master's degree in Computational Mathematics."
+        />
+        <DetailedInfo />
+      </Layout>
+    </ThemeProvider>
   );
 };
 
-export default App; 
+export default App;
