54a2611783d281b41ddf60abbc0f0412b519ce52
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2025-01-15 13:11:14 -0300

    Enhance Hero component styles: improve layout responsiveness by adjusting max-width, adding media queries for headings, and refining spacing for better visual consistency across devices.

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -5,25 +5,47 @@ export const HeroContainer = styled.div`
   flex-direction: column;
   align-items: start;
   justify-content: space-between;
-  padding: ${({ theme }) => "0 " + theme.spacing.medium};
+  max-width: 600px;
+
+  & > div {
+    max-width: 95%;
+  }
 
   * {
     margin: 0;
     padding: 0;
     font-weight: ${({ theme }) => theme.typography.weights.medium};
-    margin-top: ${({ theme }) => theme.spacing.small};
-  }
+}
 
   h1 {
     font-size: ${({ theme }) => theme.typography.sizes.xlarge};
+    margin-bottom: ${({ theme }) => theme.spacing.small};
+
+    @media (max-width: 768px) {
+      font-size: calc(${({ theme }) => theme.typography.sizes.xlarge} * 0.8);
+    }
   }
 
   h3 {
     font-size: ${({ theme }) => theme.typography.sizes.large};
+    margin-bottom: ${({ theme }) => theme.spacing.small};
+
+    @media (max-width: 768px) {
+      font-size: calc(${({ theme }) => theme.typography.sizes.large} * 0.9);
+    }
   }
 
   p {
     font-size: ${({ theme }) => theme.typography.sizes.small};
     color: ${({ theme }) => theme.colors.text.tertiary};
+    line-height: 1.6;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+    p {
+      max-width: 100%;
+    }
+    gap: ${({ theme }) => theme.spacing.medium};
   }
 `;
